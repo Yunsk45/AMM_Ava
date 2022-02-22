f1d91a2a93772b6271500372898f5eb65c325449
Farlonx <nisrecht@gmail.com>
2022-02-22 10:01:17 +0100

    Auxiliary functions + mock faucet

--- contracts/AMM.sol
+++ contracts/AMM.sol
@@ -23,4 +23,25 @@ contract AMM {
       require(_quantity <= _balance[msg.sender], "Insufficient funds.");
       _;
    }
+
+   modifier activePool() {
+      require(totalShares > 0, "Zero Liquidity");
+   }
+
+
+   function getMyHoldings() external view returns(uint256 amountToken1, uint256 amountToken2, uint256 myShare) {
+      amountToken1 = token1Balance[msg.sender];
+      amountToken2 = token2Balance[msg.sender];
+      myShare = shares[msg.sender];
+   }
+
+   function getPoolDetails() external view returns(uint256, uint256, uint256) {
+      return (totalToken1, totalToken2, totalShares);
+   }
+
+   // Mock tokens
+   function faucet(uint256 _amountToken1, uint256 _amountToken2) external {
+      token1Balance[msg.sender] = token1Balance[msg.sender].add(_amountToken1);
+      token2Balance[msg.sender] = token2Balance[msg.sender].add(_amountToken2);
+   }
 }
